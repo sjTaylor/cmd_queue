2116cb5d0c8d1a1069a7eadb2af99df45eb88478
sjTaylor <stuarttaylor7@gmail.com>
2015-09-17 12:56:23 -0500

    Cleaned up VarList

--- funs.py
+++ funs.py
@@ -5,45 +5,17 @@ import codes
 import socket
 
 class VarList:
-	def __init__(self, node=None,fromstr=None,arr=None, type='nope'):
+	def __init__(self,arr=[]):
 		#need to clean up
 		#this is code from a different project
-		self.predict = False
-		self.vals = []
-		self.predict = type is 'yes'
-		if node is not None:
-			if 'type' in node.attrib:
-				self.predict = node.attrib['type'] == 'yes'
-			if self.predict:
-				for q in node.text.split(','):
-					self.vals.append(int(q))
-			else:
-				for q in node.text.split(','):
-					self.vals.append(q)
-		elif fromstr is not None:
-			for q in node.split(','):
-				self.vals.append(q)
-		elif arr is not None:
-			self.vals=arr
+		self.vals = arr
 
 	def __getitem__(self, dex):
 		if len(self.vals) is 0:
 			return None
-		if self.predict:
-			return self.getPredictedVal(self.vals,dex)
-		return self.getCutoffVal(self.vals,dex)
-	def getCutoffVal(self, nums, index):
-		if len(nums) <= index:
-			return nums[-1]
-		return nums[index]
-	def getPredictedVal(self, nums, index):
-		if index < len(nums):
-			return nums[index]
-		if len(nums) == 1:
-			return nums[0]
-		perLevel = nums[-1] - nums[-2]
-		diff = index - len(nums) + 1
-		return nums[-1] + perLevel*diff
+		if len(self.vals) <= index:
+			return self.vals[-1]
+		return self.vals[index]		
 
 
 class Client():
